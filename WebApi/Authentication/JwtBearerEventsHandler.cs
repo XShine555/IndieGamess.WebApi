75e419ade0292f5417e2a1ac5c1228f567eccde8
XShine555 <xshine555@gmail.com>
2026-04-09 10:11:09 +0200

    Update user

--- WebApi/Authentication/JwtBearerEventsHandler.cs
+++ WebApi/Authentication/JwtBearerEventsHandler.cs
@@ -1,18 +1,25 @@
 ﻿using Application.Users.Commands;
+using Ardalis.Result;
 using Mediator;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using System.Security.Claims;
 
 namespace WebApi.Authentication
 {
-    public class JwtBearerEventsHandler(IMediator mediator)
+    public class JwtBearerEventsHandler(IMediator mediator, ILogger<JwtBearerEventsHandler> logger)
         : JwtBearerEvents
     {
         public override async Task TokenValidated(TokenValidatedContext tokenValidatedContext)
         {
             var id = tokenValidatedContext.Principal!.FindFirstValue(ClaimTypes.NameIdentifier)!;
             var username = tokenValidatedContext.Principal!.FindFirstValue(ClaimTypes.Name)!;
-            await mediator.Send(new CreateUserCommand(id, username), tokenValidatedContext.HttpContext.RequestAborted);
+
+            var createResult = await mediator.Send(new CreateUserCommand(id, username), tokenValidatedContext.HttpContext.RequestAborted);
+            if (!createResult.IsConflict())
+                return;
+
+            logger.LogInformation("User with id {Id} already exists, syncing user", id);
+            await mediator.Send(new UpdateUserCommand(id, username), tokenValidatedContext.HttpContext.RequestAborted);
         }
     }
 }
